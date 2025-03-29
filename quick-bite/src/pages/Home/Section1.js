57449768eeed0fc34cb32b93930e667b8f30a317
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-29 15:17:04 +0530

    Update Section1.js

--- quick-bite/src/pages/Home/Section1.js
+++ quick-bite/src/pages/Home/Section1.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+import Burger from "../../assets/hero/hero-2.png"
 
 function Section1() {
   return (
-    <div>Section1</div>
+    <section className="hero_section">
+        <Container>
+            <Row>
+                <Col lg={7} className="mb-5 mg-lg-0">
+                    <div className="position:relative">
+                        <img src={Burger} className="img-fluid" alt="Hero" />
+                    </div>
+                </Col>
+            </Row>
+        </Container>
+    </section>
   )
 }
 
