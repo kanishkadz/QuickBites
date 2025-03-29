59f8e2cc4ba09e3ac8ebe331fd8ebdfc2b18ac68
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-29 21:50:15 +0530

    sec3

--- quick-bite/src/pages/Home/Section3.js
+++ quick-bite/src/pages/Home/Section3.js
@@ -79,6 +79,23 @@ const mockData = [
     },
 ];
 
+//rating function
+const renderRatingIcons = (rating) => {
+    const stars = [];
+
+    for(let i=0; i<5; i++){
+        if(rating > 0.5){
+            stars.push(<i key={i} className="bi bi-star-fill"></i>);
+            rating--
+        } else if(rating > 0 && rating < 1){
+            stars.push(<i key={"half"} className="bi bi-star-fill"></i>);
+            rating--            
+        } else {
+            stars.push(<i key={`empty${i}`} className="bi bi-star-fill"></i>);
+        }
+    }
+}
+
 function Section3() {
     return (
         <section className="menu_section">
