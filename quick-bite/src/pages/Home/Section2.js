a7fde13f2fd57f836353674f22f8f935d4b3fa58
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-29 18:43:06 +0530

    Update Section2.js

--- quick-bite/src/pages/Home/Section2.js
+++ quick-bite/src/pages/Home/Section2.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Section2() {
   return (
-    <div>Section2</div>
+    <section className="about_section">
+        <Container>
+            <Row>
+                <Col lg={{span:8, offset:2}} className="text-center">
+                    <h2>The burger tastes better when you eat it with your family</h2>
+                    <p>Porta semper lacus cursus, feugiat primis ultrice a ligula risus
+                        auctor an tempus feugiat dolor lacinia cubilia curae integer
+                        orci congue and metus integer primis in integer metus
+                    </p>
+                    <Link to="/" className="btn order_now btn_red">Explore Full Menu</Link>
+                </Col>
+            </Row>
+        </Container>
+    </section>
   )
 }
 
