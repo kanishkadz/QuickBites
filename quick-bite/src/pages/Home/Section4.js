24d3f005f72df287393511db0e523610733e13c3
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-29 21:57:22 +0530

    Update Section4.js

--- quick-bite/src/pages/Home/Section4.js
+++ quick-bite/src/pages/Home/Section4.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import Promotion from "../../assets/promotion/pro.png";
 
 function Section4() {
   return (
-    <div>Section4</div>
+    <>
+        <section className="promotion">
+            <Container>
+                <Row>
+                    <Col lg={6} className="text-center mb-5 mb-lg-0">
+                        <img src={Promotion} className="img-fluid" alt="Promotion" />
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    </>
   )
 }
 
