1bb6757e764237bd01fb457e3ce7d97818e5cede
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-29 19:13:35 +0530

    Update Section3.js

--- quick-bite/src/pages/Home/Section3.js
+++ quick-bite/src/pages/Home/Section3.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';;
 
 function Section3() {
   return (
-    <div>Section3</div>
+    <section className="menu_section">
+        <Container>
+            <Row>
+                <Col lg={{span:8, offset:2}}>
+                    <h2>OUR CRAZY BURGERS</h2>
+                    <p>Aliquam a augue suscipit, luctus neque purus ipsum neque undo
+                        dolor primis libero tempus, blandit a cursus varius magna.
+                    </p>
+                </Col>
+            </Row>
+        </Container>
+    </section>
   )
 }
 
