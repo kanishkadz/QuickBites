553a5436bbf4d3bb67b7f89fe48fd191aa928fa9
Kanishka Anand <kanishkaadz@gmail.com>
2025-03-30 13:00:39 +0530

    Update Home.js

--- quick-bite/src/pages/Home/Home.js
+++ quick-bite/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ import Section2 from './Section2';
 import Section3 from './Section3';
 import Section4 from './Section4';
 import Section5 from './Section5';
-import Section6 from './Section4';
+import Section6 from './Section6';
 import Section7 from './Section7';
 
 
